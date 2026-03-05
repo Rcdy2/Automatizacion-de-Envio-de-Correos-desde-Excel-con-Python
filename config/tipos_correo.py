5c21c3d340e26b68c6b5cebf95710abaddcef47e
Rcdy2 <roccacortezdayanna@gmail.com>
2026-03-05 00:40:47 -0500

    Add files via upload

---
+++ config/tipos_correo.py
@@ -0,0 +1,12 @@
+TIPOS_CORREO = {
+    "Recordatorio": {
+        "plantilla": "plantillas/recordatorio.html",
+        "asunto_prefix": "",  
+        "descripcion": "Recordatorio amable de factura pendiente"
+    },
+    "AvisoFinal": {
+        "plantilla": "plantillas/aviso_final.html",
+        "asunto_prefix": "URGENTE - ",
+        "descripcion": "Último aviso antes de acciones legales"
+    }
+}
